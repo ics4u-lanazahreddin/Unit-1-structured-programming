6286c00dd4191f967417633dd1741ee4e18cd781
Lana <43419131+LanaZahrEddin@users.noreply.github.com>
2020-02-05 12:28:38 -0500

    added and finished day 2 unit 1

---
+++ day-2-hello-world/src/hello_world.java
@@ -0,0 +1,17 @@
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		
+		// write hello, world!
+		System.out.println("Hello,World!");
+		
+		//combine strings ( my name, my age and my school)
+		String myName = "Lana";
+		String myAge = "18 years old";
+		String mySchool = "Immaculata High School.";
+		System.out.println("My name is " + myName + ", I am " + myAge + " and I go to " + mySchool);
+		
+	   }
+
+}
