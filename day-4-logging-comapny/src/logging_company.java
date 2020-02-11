3be6a8e072a8e38a9283779a45ba9c58e9b9a53b
Lana <43419131+LanaZahrEddin@users.noreply.github.com>
2020-02-11 13:00:12 -0500

    edit

--- day-4-logging-comapny/src/logging_company.java
+++ day-4-logging-comapny/src/logging_company.java
@@ -1,9 +1,33 @@
+/*
+ * Created by: Lana ZE
+ * Created on: 11-Feb-2019
+ * Created for: ICS4U
+ * Day #3 (Logging Company)
+ * This program will calculate the number of maple logs a truck can carry with a max capacity of 1100
+ * depending on the length of the logs(the should be the same length). Options are 0.25m, 0.5m and 1m
+*/
 
 public class logging_company {
 
-	public static void main(String[] args) {
-		// 220 logs of 0.25m length
-		// 110 logs of 0.5m length
-		// 55 logs of 1m length
+	public static void main1(String[] args) {
+		// each maple log weights 20kg
+		// the maximum capacity for a truck is 1100kg
+		// 
+		static final int MAPLE_LOG_WEIGHT = 20;
+		static final int MAPLE_LOG_TRUCK_CAPACITIY = 1100;
+		static Scanner scan = new Scanner(System.in);
+		
+		public static void  main(String[] args) {
+			CalculateNumberLogs();
+		}
+	
+
+	private static void CalculateNumberLogs() {
+		// TODO Auto-generated method stub
+		int logNumber;
+		double logLength;
+		
+		
+		
 	}
 }
