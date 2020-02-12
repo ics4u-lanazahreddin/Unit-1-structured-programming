c76a8c8c541b16f093f7c5bcf489dc9ee9235ad5
Lana <43419131+LanaZahrEddin@users.noreply.github.com>
2020-02-12 13:05:43 -0500

    ;

--- day-4-logging-comapny/src/logging_company.java
+++ day-4-logging-comapny/src/logging_company.java
@@ -7,27 +7,43 @@
  * depending on the length of the logs(the should be the same length). Options are 0.25m, 0.5m and 1m
 */
 
-public class logging_company {
+import java.util.Scanner; 
 
+public class logging_company {
+	static Scanner userInput = new Scanner(System.in);
+	
 	public static void main1(String[] args) {
+	
 		// each maple log weights 20kg
 		// the maximum capacity for a truck is 1100kg
-		// 
-		static final int MAPLE_LOG_WEIGHT = 20;
-		static final int MAPLE_LOG_TRUCK_CAPACITIY = 1100;
-		static Scanner scan = new Scanner(System.in);
+		//VARIABLES
+		double weightPerLog;
+		double smallLog = .25;
+		double mediumLog = .5;
+		double largeLog = 1;
+		double numberLogs;
+		final double carryWeight = 1100;
+		final double weightOfLogs = 20;
+				
+		//initial displayed message
+		System.out.println("Please enter your maple log lenghth in meters!");
+				
+		//user input get
+		String userInputAsString = userInput.nextLine();
+		double userInputAsDouble = Double.parseDouble(userInputAsString);
+				
+		//check if the input is valid
+		if (userInputAsDouble == smallLog || userInputAsDouble == largeLog || userInputAsDouble == mediumLog) {
+		     // calculate the amount of logs a truck can carry
+		  	  weightPerLog = weightOfLogs * userInputAsDouble;
+			  numberLogs = carryWeight / weightPerLog;
+			  System.out.println("The truck could carry" + numberLogs + "logs");
+				
+		        }
 		
-		public static void  main(String[] args) {
-			CalculateNumberLogs();
-		}
-	
-
-	private static void CalculateNumberLogs() {
-		// TODO Auto-generated method stub
-		int logNumber;
-		double logLength;
-		
-		
-		
+				else {
+					// if invalid input was received
+					System.out.println("Please inter a valid number, (0.25, 0.5, 1)m");
+				}
 	}
 }
